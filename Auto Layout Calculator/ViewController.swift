e3a5ec27460ace51c093ad803f51adb9bca62bbd
kosho75 <kosho75@gmail.com>
2019-05-16 17:15:22 -0700

    complete tutorial

---
+++ Auto Layout Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Auto Layout Calculator
+//
+//  Created by 橋本 翔子 on 2019-03-14.
+//  Copyright © 2019 Shoko Hashimoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
